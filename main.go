becadf246068062dc042b8fb16084bc71677f90e
Weerasak Chongnguluam <singpor@gmail.com>
2019-03-08 15:51:57 +0700

    Finish TODO API

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -23,7 +25,8 @@ type TodoList struct {
 }
 
 func main() {
-	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	ctx := context.Background()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://usko7imkex32llnhtmyz:AvsbbVN7Dzo9jp58pPoT@bazk4bps4tqprxp-mongodb.services.clever-cloud.com:27017/bazk4bps4tqprxp"))
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
@@ -32,26 +35,100 @@ func main() {
 		log.Println(err)
 		os.Exit(1)
 	}
-	collection := client.Database("test").Collection("todolist")
+	coll := client.Database("bazk4bps4tqprxp").Collection("todos")
 	r := gin.Default()
 	r.GET("/todos", func(c *gin.Context) {
-		// LAB
+		ctx := c.Request.Context()
+		cur, err := coll.Find(ctx, bson.D{})
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		defer cur.Close(ctx)
+		var todos []TodoList
+		for cur.Next(ctx) {
+			todo := TodoList{
+				Tasks: []Task{},
+			}
+			if err := cur.Decode(&todo); err != nil {
+				c.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			todos = append(todos, todo)
+		}
+		if err := cur.Err(); err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		c.JSON(http.StatusOK, todos)
 	})
 
 	r.POST("/todos", func(c *gin.Context) {
-		// LAB
+		ctx := c.Request.Context()
+		todo := TodoList{
+			Tasks: []Task{},
+		}
+		if err := c.Bind(&todo); err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		} // id, _ := primitive.ObjectIDFromHex("aaa")
+		result, err := coll.InsertOne(ctx, todo)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		todo.ID = result.InsertedID.(primitive.ObjectID)
+		c.JSON(http.StatusOK, todo)
 	})
 
 	r.GET("/todos/:id", func(c *gin.Context) {
-		// LAB
+		ctx := c.Request.Context()
+		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
+		todo := TodoList{
+			Tasks: []Task{},
+		}
+		err := coll.FindOne(ctx, bson.D{{"_id", id}}).Decode(&todo)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				c.AbortWithError(http.StatusNotFound, err)
+			} else {
+				c.AbortWithError(http.StatusInternalServerError, err)
+			}
+			return
+		}
+		c.JSON(http.StatusOK, todo)
 	})
 
 	r.PUT("/todos/:id", func(c *gin.Context) {
-		// LAB
+		var param struct {
+			Title string `json:"title" bson:"title"`
+		}
+		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
+		if err := c.Bind(&param); err != nil {
+			return
+		}
+		_, err := coll.UpdateOne(
+			c.Request.Context(),
+			bson.D{{"_id", id}},
+			bson.D{{"$set", param}})
+
+		if err != nil {
+
+			c.AbortWithError(http.StatusInternalServerError, err)
+		}
 	})
 
 	r.DELETE("/todos/:id", func(c *gin.Context) {
-		// LAB
+		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
+		result, err := coll.DeleteOne(c.Request.Context(), bson.D{{"_id", id}})
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		if result.DeletedCount == 0 {
+			c.Status(http.StatusNotFound)
+			return
+		}
 	})
-	r.Run(":8080")
+	r.Run(":8000")
 }
