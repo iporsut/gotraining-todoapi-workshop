21fac9d112ca623bb846d30f39c508a692339b57
Weerasak Chongnguluam <singpor@gmail.com>
2019-03-18 22:04:06 +0700

    Refactor to split handler function and db action function

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.6 // indirect
 	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/tidwall/pretty v0.0.0-20180105212114-65a9db5fad51 // indirect
 	github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
--- main.go
+++ main.go
@@ -24,6 +24,14 @@ type TodoList struct {
 	Tasks []Task             `bson:"tasks" json:"tasks"`
 }
 
+type UpdateTodoListParam struct {
+	Title string `json:"title"`
+}
+
+type UpdateTaskParam struct {
+	Done bool `json:"done"`
+}
+
 func wrapError(coll *mongo.Collection, h func(context.Context, *gin.Context, *mongo.Collection) error) func(*gin.Context) {
 	return func(c *gin.Context) {
 		ctx := c.Request.Context()
@@ -64,6 +72,83 @@ func listTodoList(ctx context.Context, coll *mongo.Collection) ([]*TodoList, err
 	return todos, nil
 }
 
+func addTodoList(ctx context.Context, coll *mongo.Collection, todo *TodoList) (*TodoList, error) {
+	result, err := coll.InsertOne(ctx, todo)
+	if err != nil {
+		return nil, err
+	}
+	todo.ID = result.InsertedID.(primitive.ObjectID)
+	return todo, nil
+}
+
+func findTodoList(ctx context.Context, coll *mongo.Collection, id string) (*TodoList, error) {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	todo := &TodoList{
+		Tasks: []Task{},
+	}
+	err = coll.FindOne(ctx, bson.D{{"_id", oid}}).Decode(todo)
+	if err != nil {
+		return nil, err
+	}
+	return todo, nil
+}
+
+func updateTodoList(ctx context.Context, coll *mongo.Collection, id string, param *UpdateTodoListParam) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	_, err = coll.UpdateOne(
+		ctx,
+		bson.D{{"_id", oid}},
+		bson.D{{"$set", param}})
+	return err
+}
+
+func deleteTodoList(ctx context.Context, coll *mongo.Collection, id string) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	_, err = coll.DeleteOne(ctx, bson.D{{"_id", oid}})
+	return err
+}
+
+func addTask(ctx context.Context, coll *mongo.Collection, id string, task *Task) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	if _, err := coll.UpdateOne(
+		ctx,
+		bson.D{{"_id", oid}},
+		bson.D{{"$push",
+			bson.D{{"tasks", task}},
+		}},
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateTask(ctx context.Context, coll *mongo.Collection, id string, taskID string, param *UpdateTaskParam) error {
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	if _, err := coll.UpdateOne(
+		ctx,
+		bson.D{{"_id", oid}},
+		bson.D{{"$set", bson.D{{"tasks." + taskID + ".done", param.Done}}}},
+	); err != nil {
+		return err
+	}
+	return nil
+}
+
 func listTodoListHandler(ctx context.Context, c *gin.Context, coll *mongo.Collection) error {
 	todos, err := listTodoList(ctx, coll)
 	if err != nil {
@@ -72,6 +157,58 @@ func listTodoListHandler(ctx context.Context, c *gin.Context, coll *mongo.Collec
 
 	c.JSON(http.StatusOK, todos)
 	return nil
+}
+
+func addTodoListHandler(ctx context.Context, c *gin.Context, coll *mongo.Collection) error {
+	todo := &TodoList{
+		Tasks: []Task{},
+	}
+	if err := c.Bind(todo); err != nil {
+		return err
+	}
+	todo, err := addTodoList(ctx, coll, todo)
+	if err != nil {
+		return err
+	}
+	c.JSON(http.StatusOK, todo)
+	return nil
+}
+
+func getTodoListHandler(ctx context.Context, c *gin.Context, coll *mongo.Collection) error {
+	todo, err := findTodoList(ctx, coll, c.Param("id"))
+	if err != nil {
+		return err
+	}
+	c.JSON(http.StatusOK, todo)
+	return nil
+}
+
+func updateTodoListHandler(ctx context.Context, c *gin.Context, coll *mongo.Collection) error {
+	var param UpdateTodoListParam
+	if err := c.Bind(&param); err != nil {
+		return err
+	}
+	return updateTodoList(ctx, coll, c.Param("id"), &param)
+}
+
+func deleteTodoListHandler(ctx context.Context, c *gin.Context, coll *mongo.Collection) error {
+	return deleteTodoList(ctx, coll, c.Param("id"))
+}
+
+func addTaskHandler(ctx context.Context, c *gin.Context, coll *mongo.Collection) error {
+	var t Task
+	if err := c.Bind(&t); err != nil {
+		return err
+	}
+	return addTask(ctx, coll, c.Param("id"), &t)
+}
+
+func updateTaskHanlder(ctx context.Context, c *gin.Context, coll *mongo.Collection) error {
+	var param UpdateTaskParam
+	if err := c.Bind(&param); err != nil {
+		return err
+	}
+	return updateTask(ctx, coll, c.Param("id"), c.Param("task_id"), &param)
 }
 
 func main() {
@@ -86,114 +223,16 @@ func main() {
 		os.Exit(1)
 	}
 	coll := client.Database("bazk4bps4tqprxp").Collection("todos")
+
 	r := gin.Default()
+
 	r.GET("/todos", wrapError(coll, listTodoListHandler))
+	r.POST("/todos", wrapError(coll, addTodoListHandler))
+	r.GET("/todos/:id", wrapError(coll, getTodoListHandler))
+	r.PUT("/todos/:id", wrapError(coll, updateTodoListHandler))
+	r.DELETE("/todos/:id", wrapError(coll, deleteTodoListHandler))
+	r.POST("/todos/:id/tasks", wrapError(coll, addTaskHandler))
+	r.PUT("/todos/:id/tasks/:task_id", wrapError(coll, updateTaskHanlder))
 
-	r.POST("/todos", func(c *gin.Context) {
-		ctx := c.Request.Context()
-		todo := TodoList{
-			Tasks: []Task{},
-		}
-		if err := c.Bind(&todo); err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		} // id, _ := primitive.ObjectIDFromHex("aaa")
-		result, err := coll.InsertOne(ctx, todo)
-		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		todo.ID = result.InsertedID.(primitive.ObjectID)
-		c.JSON(http.StatusOK, todo)
-	})
-
-	r.GET("/todos/:id", func(c *gin.Context) {
-		ctx := c.Request.Context()
-		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
-		todo := TodoList{
-			Tasks: []Task{},
-		}
-		err := coll.FindOne(ctx, bson.D{{"_id", id}}).Decode(&todo)
-		if err != nil {
-			if err == mongo.ErrNoDocuments {
-				c.AbortWithError(http.StatusNotFound, err)
-			} else {
-				c.AbortWithError(http.StatusInternalServerError, err)
-			}
-			return
-		}
-		c.JSON(http.StatusOK, todo)
-	})
-
-	r.PUT("/todos/:id", func(c *gin.Context) {
-		var param struct {
-			Title string `json:"title" bson:"title"`
-		}
-		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
-		if err := c.Bind(&param); err != nil {
-			return
-		}
-		_, err := coll.UpdateOne(
-			c.Request.Context(),
-			bson.D{{"_id", id}},
-			bson.D{{"$set", param}})
-
-		if err != nil {
-
-			c.AbortWithError(http.StatusInternalServerError, err)
-		}
-	})
-
-	r.DELETE("/todos/:id", func(c *gin.Context) {
-		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
-		result, err := coll.DeleteOne(c.Request.Context(), bson.D{{"_id", id}})
-		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		if result.DeletedCount == 0 {
-			c.Status(http.StatusNotFound)
-			return
-		}
-	})
-
-	r.POST("/todos/:id/tasks", func(c *gin.Context) {
-		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
-		var t Task
-		if err := c.Bind(&t); err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		if _, err := coll.UpdateOne(
-			c.Request.Context(),
-			bson.D{{"_id", id}},
-			bson.D{{"$push",
-				bson.D{{"tasks", t}},
-			}},
-		); err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-	})
-
-	r.PUT("/todos/:id/tasks/:task_id", func(c *gin.Context) {
-		id, _ := primitive.ObjectIDFromHex(c.Param("id"))
-		var t struct {
-			Done bool `json:"done"`
-		}
-
-		if err := c.Bind(&t); err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		if _, err := coll.UpdateOne(
-			c.Request.Context(),
-			bson.D{{"_id", id}},
-			bson.D{{"$set", bson.D{{"tasks." + c.Param("task_id") + ".done", t.Done}}}},
-		); err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-	})
 	r.Run(":8000")
 }
